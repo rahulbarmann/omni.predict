f6e59915a17f415f2b2d88a2574edcc542d36caa
rahulbarmann <itsrahulbarman1@gmail.com>
2024-11-15 23:16:58 +0700

    feat: add privy EOA integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,39 @@
-import Image from "next/image";
+"use client";
+
+import { usePrivy, useWallets } from "@privy-io/react-auth";
 
 export default function Home() {
-    return <>Hello</>;
+    const { ready, authenticated, login, logout, user, linkGoogle } =
+        usePrivy();
+    const { wallets } = useWallets();
+
+    function LogoutButton() {
+        const disableLogout = !ready || (ready && !authenticated);
+
+        return (
+            <button disabled={disableLogout} onClick={logout}>
+                Log out
+            </button>
+        );
+    }
+
+    function LoginButton() {
+        const disableLogin = !ready || (ready && authenticated);
+
+        return (
+            <button disabled={disableLogin} onClick={login}>
+                Log in
+            </button>
+        );
+    }
+    console.log("Wallets", wallets);
+
+    return (
+        <>
+            <div>Wallet: {user?.wallet ? user?.wallet.address : "None"}</div>
+            <LoginButton />
+            <LogoutButton />
+            <div className="text-7xl">Helloooo</div>
+        </>
+    );
 }
